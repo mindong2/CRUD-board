b59f288b582e601488e15b462486ba29c6ff27e3
Dongmin Kim <qlsnaos12@naver.com>
2023-03-16 11:52:35 +0900

    Suspense, lazy로 초기 렌더링시간 감축

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { Spinner } from "react-bootstrap";
 import { loginUser, clearUser } from "./Reducer/userSlice";
 import firebase from "./firebase";
 import Heading from "./Component/Heading";
-import Upload from "./Component/Post/Upload";
-import PostArea from "./Component/Post/PostArea";
-import Edit from "./Component/Post/Edit";
-import Login from "./Component/User/Login";
-import Register from "./Component/User/Register";
-import Mypage from "./Component/User/Mypage";
-import Mainpage from "./Component/Mainpage";
 import GlobalStyle from "./Style/GlobalStyle";
+
+// 첫 렌더링 시간이 너무 길어 lazy 로딩
+const Upload = lazy(() => import("./Component/Post/Upload"));
+const PostArea = lazy(() => import("./Component/Post/PostArea"));
+const Edit = lazy(() => import("./Component/Post/Edit"));
+const Login = lazy(() => import("./Component/User/Login"));
+const Register = lazy(() => import("./Component/User/Register"));
+const Mypage = lazy(() => import("./Component/User/Mypage"));
+const Mainpage = lazy(() => import("./Component/Mainpage"));
+
 const App = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    // firebase 로그인 하지 않았을때 userInfo -> null 반대는 userInfo 보여줌
-    useEffect(() => {
-        firebase.auth().onAuthStateChanged((userInfo) => {
-            if (userInfo !== null) {
-                dispatch(loginUser(userInfo.multiFactor.user));
-            } else {
-                dispatch(clearUser());
-            }
-        });
-    }, []);
+  // firebase 로그인 하지 않았을때 userInfo -> null 반대는 userInfo 보여줌
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((userInfo) => {
+      if (userInfo !== null) {
+        dispatch(loginUser(userInfo.multiFactor.user));
+      } else {
+        dispatch(clearUser());
+      }
+    });
+  }, []);
 
-    return (
-        <div className="App">
-            <GlobalStyle />
-            <Heading />
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Heading />
 
-            <Routes>
-                <Route path="/" element={<Mainpage />}></Route>
-                <Route path="/upload" element={<Upload />}></Route>
+      <Suspense fallback= {<Spinner/>}>
+        <Routes>
+          <Route path="/" element={<Mainpage />}></Route>
+          <Route path="/upload" element={<Upload />}></Route>
 
-                <Route path="/post/:postNum" element={<PostArea />}></Route>
-                <Route path="/edit/:postNum" element={<Edit />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/mypage" element={<Mypage />}></Route>
-                <Route path="/register" element={<Register />}></Route>
-                {/* <Route path="*" element={<div>404 알수없는 페이지</div>}></Route> */}
-            </Routes>
-        </div>
-    );
+          <Route path="/post/:postNum" element={<PostArea />}></Route>
+          <Route path="/edit/:postNum" element={<Edit />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/mypage" element={<Mypage />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          {/* <Route path="*" element={<div>404 알수없는 페이지</div>}></Route> */}
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
